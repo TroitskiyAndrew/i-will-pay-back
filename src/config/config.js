52bb66de59854f3d7ada9d81e497cb6d903ba814
AndreyTroitskiy <TroitskiyA@yandex.ru>
2025-09-12 20:54:59 +0400

    fix: header

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,6 @@ module.exports = {
     mongodbConnectionString: process.env.MONGODB_CONNECTION_STRING || '',
     frontURL: process.env.FRONT_URL || "*",
     botToken: process.env.BOT_TOKEN  || "",
-    telegrammHeader: process.env.BOT_TOKEN  || "",
+    telegrammHeader: process.env.TELEGRAMM_HEADER  || "",
     prod: process.env.LOCAL_DEVELOPMENT  == null,
 }
