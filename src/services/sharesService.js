4a8d06a173a0d2ee69488610b283b4dadf4f62a8
AndreyTroitskiy <TroitskiyA@yandex.ru>
2025-09-17 15:55:58 +0400

    feat: callback data

--- src/services/sharesService.js
+++ src/services/sharesService.js
@@ -37,7 +37,7 @@ async function senMessageAboutNewShare(userId, payment, newShare) {
             })
             reply_markup.inline_keyboard[0].push({
                 text: 'Подтвердить сумму',
-                callback_data: `acceptShare&${newShare.id}`
+                callback_data: `acceptShareByUser&${newShare.id}`
             })
         } else {
             reply_markup.inline_keyboard[0].push({
