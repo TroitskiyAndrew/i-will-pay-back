464d1f14f0e1f9a714c1f730ee099dc97d045852
AndreyTroitskiy <TroitskiyA@yandex.ru>
2025-09-18 12:14:29 +0400

    fix: mok user

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { tgApiUrl } = require("../config/config");
 
 async function sendMessage(chat_id, text, reply_markup) {
-    if(chat_id !== 111){
+    if(chat_id === 111){
         return;
     }
     await axios.post(`${tgApiUrl}/sendMessage`, {
